ce207cab2e603a1e3f2f0cf961134836778d4ad5
ranyuo <ranjoumisael@gmail.com>
2025-10-08 16:15:48 -0500

    ok
    ok

--- edu_partner_document/controllers/main.py
+++ edu_partner_document/controllers/main.py
@@ -12,17 +12,20 @@ class WebsitePartnerDocument(http.Controller):
     def partner_document_list(self, page=1, **kwargs):
         search = kwargs.get('search', '')
         domain = []
+        partner_documents = []
+        pages = 1
+        total = 0
 
         if search:
             domain = ['|','|', ('name', 'ilike', search), ('partner_code', '=', search), ('partner_vat', '=', search)]
 
-        page_size = 10
-        PartnerDocument = request.env['edu.partner.document'].sudo()
-        total = PartnerDocument.search_count(domain)
-        pages = math.ceil(total / page_size)
+            page_size = 10
+            PartnerDocument = request.env['edu.partner.document'].sudo()
+            total = PartnerDocument.search_count(domain)
+            pages = math.ceil(total / page_size)
 
-        offset = (page - 1) * page_size
-        partner_documents = PartnerDocument.search(domain, limit=page_size, offset=offset)
+            offset = (page - 1) * page_size
+            partner_documents = PartnerDocument.search(domain, limit=page_size, offset=offset)
 
         return request.render("edu_partner_document.partner_document_page", {
             'partner_documents': partner_documents,
