01b2c7658c4cb64a07c11f17152cb4e29abcbd9e
ranyuo <149195625+ranyuo@users.noreply.github.com>
2025-11-01 19:01:48 -0500

    Add PDF download for helpdesk ticket search

--- helpdesk_website_extension/controllers/main.py
+++ helpdesk_website_extension/controllers/main.py
@@ -42,3 +42,32 @@ class WebsiteHelpdeskTicket(http.Controller):
                 'helpdesk_ticket': ticket,
             },
         )
+
+    @http.route(
+        '/helpdesk-ticket/<int:ticket_id>/pdf',
+        type='http',
+        auth="public",
+        website=True,
+    )
+    def helpdesk_ticket_pdf(self, ticket_id, **kwargs):
+        HelpdeskTicket = request.env['helpdesk.ticket'].sudo()
+        ticket = HelpdeskTicket.browse(ticket_id)
+        if not ticket.exists():
+            return request.not_found()
+
+        report = request.env.ref('helpdesk_website_extension.action_report_helpdesk_ticket').sudo()
+        pdf_content, _ = report._render_qweb_pdf([ticket.id])
+
+        filename = "Expediente-%s" % (ticket.ticket_ref or ticket.name or ticket.id)
+        filename = str(filename).replace('/', '-').replace('\\', '-')
+
+        pdfhttpheaders = [
+            ('Content-Type', 'application/pdf'),
+            ('Content-Length', len(pdf_content)),
+            (
+                'Content-Disposition',
+                'attachment; filename="%s.pdf"' % filename,
+            ),
+        ]
+
+        return request.make_response(pdf_content, headers=pdfhttpheaders)
